3a8b7561e9e880ed926c3093cbf14d87012bd29d
Monik9 <monikinderjitsingh5@gmail.com>
2020-05-20 20:01:04 +0530

    pre final commit

---
+++ lib/helpers/custom_route.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class CustomRoute extends MaterialPageRoute {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
